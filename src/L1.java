443212dbd25475a6794170a865ebbfc8b4ebd568
pattojul000 <pattojul000@warren.k12.in.us>
2021-08-04 12:52:02 -0400

    Completed L1 and L2 Notes

--- src/L1.java
+++ src/L1.java
@@ -1,8 +1,12 @@
 public class L1 {
 
-    //You will notice that the class header is already define.
+    public static void main(String[] args) {
 
-    //add the main method header below.
+        System.out.println("Hello World!");
+        System.out.print("Hello!");
+        System.out.println("Hello World!");
 
 
+
+    }
 }
--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,22 @@
 public class L2 {
 
-    //add the main method header
+
+    public static void main(String[] args){
+        double num1 = 3.4;
+        boolean num2 = false;
+        String num3 = "4.5";
+
+        String firstName = "George";
+        int age = 16;
+        System.out.println(firstName);
+        System.out.println(age);
+
+        String accountName = "Julien";
+        int accountNumber = 243345812;
+        double balance = 532.45;
+
+        System.out.println(accountName);
+        System.out.println(accountNumber);
+        System.out.println(balance);
+    }
 }
