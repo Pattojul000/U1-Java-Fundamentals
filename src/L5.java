54906a12a78afb020cac94369bbfb64406d0d4d9
pattojul000 <pattojul000@warren.k12.in.us>
2021-08-09 12:21:12 -0400

    Completed L3, L4, L5

--- src/L5.java
+++ src/L5.java
@@ -1,2 +1,19 @@
 public class L5 {
+
+    public static void main(String[] args){
+
+        /*double basePay = 7.25;
+        double hoursWeek1 = 16.5;
+        double hoursWeek2 = 23.75;
+
+        double totalHours = hoursWeek1 + hoursWeek2;
+        double wages = basePay * totalHours;
+
+        System.out.println("Your bi-weekly paycheck is: $" + wages);*/
+
+        double fraction = 1/2.0;
+        System.out.println(fraction);
+
+    }
+
 }
